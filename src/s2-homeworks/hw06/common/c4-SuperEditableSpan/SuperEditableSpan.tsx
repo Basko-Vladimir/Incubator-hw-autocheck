eb8b97d526dec6757fdbe7cbedd73eca5d6c1a7e
Vladimir Basko <1989bvg@gmail.com>
2024-01-30 12:02:46 +0300

    completed HW6

--- src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
+++ src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
@@ -1,3 +1,5 @@
+// noinspection LanguageDetectionInspection
+
 import React, {
     DetailedHTMLProps,
     InputHTMLAttributes,
@@ -42,19 +44,19 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
 
     const onEnterCallback = () => {
         // выключить editMode при нажатии Enter // делают студенты
-
+        setEditMode(false);
         onEnter?.()
     }
     const onBlurCallback = (e: React.FocusEvent<HTMLInputElement>) => {
         // выключить editMode при нажатии за пределами инпута // делают студенты
-
+        setEditMode(false);
         onBlur?.(e)
     }
     const onDoubleClickCallBack = (
         e: React.MouseEvent<HTMLSpanElement, MouseEvent>
     ) => {
         // включить editMode при двойном клике // делают студенты
-
+        setEditMode(true);
         onDoubleClick?.(e)
     }
 
