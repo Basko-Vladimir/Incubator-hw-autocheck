1596eeb7426fc7f83be46a362bad354d1206d2a1
Vladimir Basko <1989bvg@gmail.com>
2024-01-30 11:42:25 +0300

    adjusted HW5 according to requirements

--- src/s2-homeworks/hw05/pages/Junior.tsx
+++ src/s2-homeworks/hw05/pages/Junior.tsx
@@ -7,6 +7,7 @@ import React from 'react'
 function Junior() {
     return (
         <div id={'hw5-page-junior'}>
+            Junior page
             {/*<HW6 />*/}
             {/*<HW7 />*/}
             {/*<HW8 />*/}
