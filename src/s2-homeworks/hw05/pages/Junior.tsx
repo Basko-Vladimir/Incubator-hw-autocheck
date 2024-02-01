de16d4a6381624388bd26436719cb4798f1419d7
Vladimir Basko <1989bvg@gmail.com>
2024-02-01 16:35:30 +0300

    completed hw8, lesson 12

--- src/s2-homeworks/hw05/pages/Junior.tsx
+++ src/s2-homeworks/hw05/pages/Junior.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import HW7 from "../../hw07/HW7";
+import HW8 from "../../hw08/HW8";
 
 function Junior() {
     return (
         <div id={'hw5-page-junior'}>
             Junior page
             {/*<HW6 />*/}
-            <HW7 />
-            {/*<HW8 />*/}
+            {/*<HW7 />*/}
+            <HW8 />
             {/*<HW9 />*/}
         </div>
     )
